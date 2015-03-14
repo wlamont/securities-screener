9a924b9ee4b6cb9b2d048c409188fe03d58706f8
Warren Lamont <warren.lamont@gmail.com>
2015-03-14 12:43:52 -0400

    Added some error trapping and handling, especially if price is none

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Security (stock) Screener
 
-Want to invest like Warren Buffett?  Yeah, me too.  Well we can't.  But we can use some of the methodology Warren's teacher and mentor Benjamin Graham imparted on Warren and is available in the book "The Intelligent Investor".  Here are two files that helps screen some of the basic stocks.  
+Want to invest like Warren Buffett?  Yeah, me too.  Well we can't.  But we can use some of the methodology Warren's teacher and mentor Benjamin Graham imparted on Warren and is available in the book "The Intelligent Investor".  Here are two files that help screen securities using the Graham methodology.  
+
+Methodology #1:
 
 ``For the defensive investor we suggested an upper limit of purchase price at 25 times average earnings [per share] of the past seven years.'' p. 159 from "The Intelligent Investor"
 
 stock_analysis_01.py 
 
-Uses beautifulsoup to scrape html financial data for each company in the S&P500.  Reads in the last 5 years worth of EPS plus the current price. Outputs the stock symbol, current price, 5 yr EPS average, max price I would pay based on 25 x (5 yr AVG of EPS) and outputs a ratio of the price / max (smaller numbers are better)
+Uses beautifulsoup to scrape html financial data for each company in the S&P500.  Reads in the last 5 years worth of EPS plus the current price. Outputs the stock symbol, current price, 5 yr EPS average, max price one would pay based on 25 x (5 yr AVG of EPS) and outputs a ratio of the price / max (smaller numbers are better)
 
+Methodology #2:
 
 ``In general, a price/earnings ratio (or "P/E" ratio) below 10 is considered low, between 10 and 20 is considered moderate, and greater than 20 is considered expensive." p. 70 
 
@@ -17,7 +20,7 @@ and
 
 "The conservative investor should concentrate on issues selling at no more than 1.3 of book value (P/BV < 1.3)." p. 199
 
-This conservatie investor therefore wants to concentrate on stocks with the following financial characteristics P/E * P/BV < 22.5 (price to earnings muliplied by price to book)
+This conservative investor therefore wants to concentrate on stocks with the following financial characteristics P/E * P/BV < 22.5 (price to earnings muliplied by price to book)
 
 stock_analysis_BV_02.py
 
--- stock_analysis_BV_02.py
+++ stock_analysis_BV_02.py
@@ -57,48 +57,62 @@ def buffett_score(ticker_symbol):
     url = 'http://www.marketwatch.com/investing/stock/'
     full_url = url + ticker_symbol + '/profile'
 #    print full_url
-    text_soup = BeautifulSoup(urlopen(full_url).read()) #read in
-#    print text_soup
-    price = text_soup.find('p', {'class': 'data bgLast'})
-    price = price.string.replace(',', '') #trap for comma in price
-#    print 'Current/Last Price:', price.string
-    titles = text_soup.findAll('p', {'class': 'column'})
-#    print titles
-    flag1 = 0   #use these to trap if P/E or PBR do not exist like for CRS
-    flag2 = 0 
-    for title in titles:
-#        print title
-        if 'P/E Current' in title.text:
-            for PE in title.findNextSiblings(attrs={'class': 'data lastcolumn'}):
-                pe = PE.text
-                pe = pe.replace(',','')  # if there is a comma, remove it
-                flag1 = 1
-                
-        if 'Price to Book Ratio' in title.text:
-            for PBR in title.findNextSiblings(attrs={'class': 'data lastcolumn'}):
-                pbr = PBR.text
-                pbr = pbr.replace(',', '') 
-                flag2 = 1
-    if 'pbr' not in locals():
-        pbr = 0.0
-    if 'pe' not in locals():
-        pe = 0.0
+    try:
+        text_soup = BeautifulSoup(urlopen(full_url).read()) #read in
+    except:
+        print 'An error has occured reading this stock'
+        pass
         
-    if flag1 == 0:
-        pe = np.nan
-    if flag2 == 0:
-        pbr = np.nan
-#    print pe
-    #trapping for negative PE
-    if flag1 == 1:
-        if pe[0] == '-':
-            pe = 0
-        elif pe[1] == ',':
-            pe = pe[0] + pe[2:]
-    # using price.replace above to trap for comma
-#    if price.string[1] == ',':
-#        price.string =  price.string[0] + price.string[2:]
-    return float(price), float(pe), float(pbr), float(pe) * float(pbr)
+        
+#    print text_soup
+
+    price = text_soup.find('p', {'class': 'data bgLast'})
+    
+#    print price
+#    print type(price)
+    if price is None:
+        print '<<< price is None >>>', 
+        return 0, 0, 0, 0
+    else:
+        price = price.string.replace(',', '') #trap for comma in price
+    #    print 'Current/Last Price:', price.string
+        titles = text_soup.findAll('p', {'class': 'column'})
+    #    print titles
+        flag1 = 0   #use these to trap if P/E or PBR do not exist like for CRS
+        flag2 = 0 
+        for title in titles:
+    #        print title
+            if 'P/E Current' in title.text:
+                for PE in title.findNextSiblings(attrs={'class': 'data lastcolumn'}):
+                    pe = PE.text
+                    pe = pe.replace(',','')  # if there is a comma, remove it
+                    flag1 = 1
+                    
+            if 'Price to Book Ratio' in title.text:
+                for PBR in title.findNextSiblings(attrs={'class': 'data lastcolumn'}):
+                    pbr = PBR.text
+                    pbr = pbr.replace(',', '') 
+                    flag2 = 1
+        if 'pbr' not in locals():
+            pbr = 0.0
+        if 'pe' not in locals():
+            pe = 0.0
+            
+        if flag1 == 0:
+            pe = np.nan
+        if flag2 == 0:
+            pbr = np.nan
+    #    print pe
+        #trapping for negative PE
+        if flag1 == 1:
+            if pe[0] == '-':
+                pe = 0
+            elif pe[1] == ',':
+                pe = pe[0] + pe[2:]
+        # using price.replace above to trap for comma
+    #    if price.string[1] == ',':
+    #        price.string =  price.string[0] + price.string[2:]
+        return float(price), float(pe), float(pbr), float(pe) * float(pbr)
 
 
 if __name__ == '__main__':
@@ -114,7 +128,8 @@ if __name__ == '__main__':
 #    ifile = open('sp400.csv','r') #S&P 400 Mid Cap
 #    ifile = open('sp600.csv','r') #S&P 400 Mid Cap
     
-    OFILE = open('data_buffett_largecap_20150308.txt', 'w')
+#    OFILE = open('test_20150314.txt', 'w')
+    OFILE = open('data_buffett_largecap_20150314.txt', 'w')
 #    OFILE = open('data_buffett_midcap_20140503.txt', 'w')
 #    OFILE = open('data_buffett_smallcap_20140503.txt', 'w')
     
@@ -122,6 +137,8 @@ if __name__ == '__main__':
         sym = line
         sym.strip('\r\n')
         symbol.append(str(sym[0:(len(sym)-1)]))  
+    
+#    symbol = ['PETM']
     
 #    print symbol     
     i = 0
